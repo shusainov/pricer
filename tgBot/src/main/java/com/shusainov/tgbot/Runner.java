bf239d369f768b80154e66ae2d20f542284be651
shusainov <shusainov3@gmail.com>
2022-06-01 17:24:36 +0500

    TGBot add config file

--- tgBot/src/main/java/com/shusainov/tgbot/Runner.java
+++ tgBot/src/main/java/com/shusainov/tgbot/Runner.java
@@ -1,8 +1,10 @@
 package com.shusainov.tgbot;
 
+import com.shusainov.tgbot.utils.Config;
+
 public class Runner {
     public static void main(String[] args) {
-        Updater updater = new Updater("15443541071:AAHUemb7fDkPhzE3r33Vrq2o3DJGLV7Eqac");
+        Updater updater = new Updater(Config.get("TOKEN"));
         System.out.println(updater.checkMyStatus());
     }
 }
---
+++ tgBot/src/main/java/com/shusainov/tgbot/utils/Config.java
@@ -0,0 +1,37 @@
+package com.shusainov.tgbot.utils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Берёт конфиг по следующим путям:
+ * Если находит дальше не ищет
+ * 1.-Dconfig=config_file_path
+ * 2.config/config.properties
+ * 3.src/main/resources/config.properties
+ */
+public class Config {
+
+    private static Properties PROPERTY;
+
+    public static String get(String parameter) {
+        if (PROPERTY == null) {
+            try {
+                String configPath =System.getProperty("config");
+                if (configPath==null||(!new File(configPath).isFile())) {
+                    configPath = "config/config.properties";
+                }
+
+                if (!new File(configPath).isFile()) {
+                    configPath = "src/main/resources/config.properties";
+                }
+                PROPERTY = new Properties();
+                PROPERTY.load(new FileInputStream(configPath));
+            } catch (IOException e) {
+            }
+        }
+        return PROPERTY.getProperty(parameter);
+    }
+}
